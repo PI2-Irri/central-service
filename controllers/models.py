e8fdca1d76bd9db4d883c7eab623d19732373ef3
Bruna Pinos <bruna.pinos@gmail.com>
2019-09-30 09:10:43 -0300

    Creates controller class
    Co-authored-by: Guilherme Augusto <guilherme.francais@gmail.com>

--- controllers/models.py
+++ controllers/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Controller(models.Model):
+    name = models.CharField(max_length=25)
+    is_valid = models.BooleanField(default=False)
+    token = models.CharField(max_length=10, unique=True)
+
