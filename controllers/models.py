b9632d01cad09dea9d096c0a62ea573e78047432
Guilherme Augusto <guilherme.francais@gmail.com>
2019-10-02 01:35:41 -0300

    Limits controller creation only when authenticated

--- controllers/models.py
+++ controllers/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from users.models import CustomUser
 
 
 class Controller(models.Model):
     name = models.CharField(max_length=25)
     is_valid = models.BooleanField(default=False)
     token = models.CharField(max_length=10, unique=True)
+
+    owner = models.ManyToManyField(CustomUser, related_name='users')
--- controllers/views.py
+++ controllers/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import permissions
-from .models import Controller 
+from .models import Controller
 from .serializers import ControllerSerializer
 
 
 class ControllerViewSet(viewsets.ModelViewSet):
     queryset = Controller.objects.all()
     serializer_class = ControllerSerializer
-    permission_classes = (permissions.AllowAny,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
