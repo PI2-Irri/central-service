ee209458462246fd862d1233805cde3ed910948d
Bruna Pinos <bruna.pinos@gmail.com>
2019-10-15 01:42:09 -0300

    Implements first structure of api and controller synchronization
    Co-authored-by: Guilherme Augusto <guilherme.francais@gmail.com>

--- controllers/serializers.py
+++ controllers/serializers.py
@@ -24,9 +24,9 @@ class ControllerSerializer(serializers.HyperlinkedModelSerializer):
         ip = validated_data.get('ip_address')
         token = validated_data.get('token')
         # try:
-            # ControllerCommunication.check_token(token, ip_address) 
+        #     ControllerCommunication.check_token(token, ip)
         # except ControllerTokenException:
-            # raise APIException({"error": "Token does not match with ip token"})
+        #     raise APIException({"error": "Token does not match with ip token"})
 
         controller = Controller(
             name=validated_data.get('name'),
@@ -38,12 +38,14 @@ class ControllerSerializer(serializers.HyperlinkedModelSerializer):
         controller.save()
         controller.owner.add(validated_data.get('owner'))
 
-        modules = ControllerCommunication.get_controller_modules(token, ip)
+        modules = ControllerCommunication.get_controller_modules(
+            token, 'http://' + ip + controller.port
+        )
 
         for module in modules:
             Module.objects.create(
                 rf_address=module["rf_address"],
-                controller=controller 
+                controller=controller
             )
 
         return controller
