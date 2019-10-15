8e93fff519d306dbda6cf9723f37ed72dab72ef4
Guilherme Augusto <guilherme.francais@gmail.com>
2019-10-15 01:40:41 -0300

    Implements filtered collection of module belongs to user
    Co-authored-by: Bruna Pinos <bruna.pinos@gmail.com>

--- modules/views.py
+++ modules/views.py
@@ -3,11 +3,26 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.authentication import TokenAuthentication
 from .models import Module
+from controllers.models import Controller
 from .serializers import ModuleSerializer
 
 
 class ModuleViewSet(viewsets.ModelViewSet):
     queryset = Module.objects.none()
+    model = Module
     serializer_class = ModuleSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     authentication_classes = (TokenAuthentication,)
+
+    def get_queryset(self):
+        user = self.request.user
+
+        controllers = Controller.objects.filter(
+            owner=user
+        )
+
+        modules = self.model.objects.filter(
+            controller__in=controllers
+        )
+
+        return modules
