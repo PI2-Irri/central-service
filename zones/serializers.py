9e7a0059582da6a463f5167b4549699b3c4579c1
Guilherme Augusto <guilherme.francais@gmail.com>
2019-11-05 20:45:39 -0300

    Implements zone custom creation

--- zones/serializers.py
+++ zones/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+from rest_framework.exceptions import APIException
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Controller
 from .models import Zone
 
 
@@ -15,3 +18,38 @@ class ZoneSerializer(serializers.HyperlinkedModelSerializer):
             'controller',
             'url'
         )
+
+    def to_internal_value(self, data):
+        if isinstance(data.get('controller'), str):
+            return data
+        else:
+            raise APIException(
+                {'error': 'Token information has a invalid type.'}
+            )
+
+    def create(self, validated_data):
+        token = validated_data.get('controller')
+        zone = None
+
+        if not token:
+            raise APIException(
+                {'detail': 'Token must be a sended.'}
+            )
+
+        try:
+            controller = Controller.objects.get(
+                token=token
+            )
+            zone = Zone.objects.create(
+                name=validated_data.get('name'),
+                zip=validated_data.get('zip'),
+                latitude=validated_data.get('latitude'),
+                longitude=validated_data.get('longitude'),
+                controller=controller
+            )
+        except Controller.DoesNotExist as exception:
+            raise APIException(
+                {'error': 'This token does not match with any controller.'}
+            )
+
+        return zone
