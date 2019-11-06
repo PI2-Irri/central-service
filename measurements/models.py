83257607fd003eec424e18f2bfb27622b7cc43fa
Bruna Pinos <bruna.pinos@gmail.com>
2019-11-06 15:52:07 -0300

    Creates ZoneMeasurements class

--- measurements/models.py
+++ measurements/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from controllers.models import Controller
 from modules.models import Module
-
+from zones.models import Zone
 
 class ActuatorsMeasurement(models.Model):
     water_consumption = models.FloatField(default=0.0)
@@ -15,3 +15,10 @@ class ModulesMeasurement(models.Model):
     battery_level = models.IntegerField(default=0)
 
     module = models.ForeignKey(Module, on_delete=models.CASCADE)
+
+class ZoneMeasurement(models.Model):
+    soil_temperature = models.FloatField(default=0.0)
+    air_temperature = models.FloatField(default=0.0)
+    precipitation = models.FloatField(default=0.0)
+
+    zone = models.ForeignKey(Zone, on_delete=models.CASCADE)
