bc8bb710bcae36dbc873db5fb2c4d31223281c2d
Guilherme Augusto <guilherme.francais@gmail.com>
2019-11-08 14:07:57 -0300

    Fixes module creation

--- modules/models.py
+++ modules/models.py
@@ -4,5 +4,4 @@ from controllers.models import Controller
 
 class Module(models.Model):
     rf_address = models.CharField(max_length=30, unique=True)
-
     controller = models.ForeignKey(Controller, on_delete=models.CASCADE)
