2133b0143be9210e4284ee5748d092e392a319c0
Guilherme Augusto <guilherme.francais@gmail.com>
2019-11-13 16:27:14 -0300

    Fixes some modules informations

--- modules/models.py
+++ modules/models.py
@@ -3,5 +3,8 @@ from controllers.models import Controller
 
 
 class Module(models.Model):
-    rf_address = models.CharField(max_length=30, unique=True)
+    rf_address = models.IntegerField(unique=True)
     controller = models.ForeignKey(Controller, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.rf_address
